9257db6ea453ca1af964eb7463f8d278dbff64d3
sol naranjo <sol9naranjo@gmail.com>
2021-01-10 22:21:19 -0300

    Punto Dulce

--- index.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    console.log('la pagina esta cargada')
-  ​
-    const button = document.querySelector('#click-me')
-    button.addEventListener('click', () => {
-      console.log('Hiciste click en el boton!')
-    })
-  })
-
